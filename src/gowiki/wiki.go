15a1b5dffe88c9f7a8eef0f67bb2b4bec08e2935
Evan <evan@datarank.com>
2015-10-06 18:04:52 -0500

    Created view and save handlers

--- src/gowiki/wiki.go
+++ src/gowiki/wiki.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "html/template"
     "fmt"
     "io/ioutil"
     "net/http"
@@ -30,8 +31,38 @@ func loadPage(title string) (*Page, error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
-    p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    p, err := loadPage(title)
+
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+
+    renderTemplate(w, "view", p)
+
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    p.save()
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter,tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
 }
 
 func main() {
@@ -46,6 +77,9 @@ func main() {
     // go build wiki.go
     // ./wiki
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
+
     http.ListenAndServe(":8080", nil)
 }
 
